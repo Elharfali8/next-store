dbc0499ddfa8cdf11c25ccac279a926951761a3c
Elharfali <0631173826you@gmail.com>
2024-10-11 11:30:53 +0100

    Add Authentication with Clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Navbar from "@/components/navbar/Navbar";
 import Container from "@/components/global/Container";
 import Providers from "./providers";
+import {ClerkProvider} from '@clerk/nextjs'
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -27,6 +28,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider>
     <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
@@ -38,6 +40,7 @@ export default function RootLayout({
           </Container>
         </Providers>
       </body>
-    </html>
+      </html>
+      </ClerkProvider>
   );
 }
--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import { LuUser2 } from "react-icons/lu"
+import { currentUser } from "@clerk/nextjs/server"
 
-function UserIcon() {
+async function UserIcon() {
+  const user = await currentUser()
+
+  const profileImage = user?.imageUrl
+
+  if (profileImage) {
+    return <img src={profileImage} className="w-6 h-6 rounded-full object-cover" alt="profile" />
+  }
+
   return (
-    <div>UserIcon</div>
+    <LuUser2 className="w-6 h-6 bg-primary rounded-full text-white" />
   )
 }
 
