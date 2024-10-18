68350c04400806d1dfbe37832a9a147ebd07b11a
Elharfali <0631173826you@gmail.com>
2024-10-18 11:16:38 +0100

    Admin page : add create product page, create product action -part 1

---
+++ app/admin/products/[id]/edit/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function EditProductPage() {
+  return (
+    <div>EditProductPage</div>
+  )w
+}
+
+export default EditProductPage
--- components/navbar/LinksDropdown.tsx
+++ components/navbar/LinksDropdown.tsx
@@ -12,8 +12,12 @@ import { links } from '@/utils/links';
 import UserIcon from './UserIcon';
 import { SignedIn, SignedOut, SignInButton, SignUpButton } from '@clerk/nextjs';
 import SignOutLink from './SignOutLink';
+import { auth } from '@clerk/nextjs/server';
 
 function LinksDropdown() {
+  const { userId } = auth()
+  const isAdmin = userId === process.env.ADMIN_USER_ID
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
@@ -37,7 +41,8 @@ function LinksDropdown() {
           </DropdownMenuItem>
         </SignedOut>
         <SignedIn>
-        {links.map((link) => {
+          {links.map((link) => {
+          if (link.label === 'dashboard' && !isAdmin) return null
           return <DropdownMenuItem key={link.href}>
             <Link href={link.href} className=' capitalize w-full'>
               {link.label}
